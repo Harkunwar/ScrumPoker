f62d91b8b40c35873b3cd4e71cf7854e7475362a
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-05-23 15:58:16 -0700

    move to folder

--- api/server/[server].ts
+++ api/server/[server].ts
@@ -1,5 +1,5 @@
-import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
+import trpc from "@trpc/server";
+import trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
 
 const appRouter = trpc.router().query("hello", {
