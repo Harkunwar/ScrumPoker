33dcb62624e8ad2c8254ddccabcf463667caf133
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-05-23 15:56:48 -0700

    api update

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,25 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+import * as trpc from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+import { z } from "zod";
 
-export default function (req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
-}
+const appRouter = trpc.router().query("hello", {
+  input: z
+    .object({
+      text: z.string().nullish(),
+    })
+    .nullish(),
+  resolve({ input }) {
+    return {
+      greeting: `hello ${input?.text ?? "world"}`,
+    };
+  },
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
